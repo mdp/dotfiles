4221c43c60fad8eff996a08933166d0122c4fca1
Mark Percival <m@mdp.im>
2024-01-10 10:59:01 -0500

    Gitsign

--- private_dot_config/nvim/lua/mdp/plugins/neotree.lua
+++ private_dot_config/nvim/lua/mdp/plugins/neotree.lua
@@ -6,5 +6,16 @@ return {
       "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
       "MunifTanjim/nui.nvim",
       -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
-    }
+    },
+    config = function()
+      require("neo-tree").setup {}
+
+      -- set keymaps
+      local keymap = vim.keymap -- for conciseness
+
+      keymap.set("n", "<leader>tt", "<cmd>Neotree toggle<cr>", { desc = "Toggle NeoTree" })
+      keymap.set("n", "<leader>td", "<cmd>Neotree close<cr>", { desc = "NeoTree close" })
+
+    end,
+
 }
