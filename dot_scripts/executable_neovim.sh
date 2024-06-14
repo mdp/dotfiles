e996d669eb0f96d2a682eda30965377df983e0a0
Mark Percival <m@mdp.im>
2024-06-14 06:40:41 +0000

    Update

--- dot_scripts/executable_neovim.sh
+++ dot_scripts/executable_neovim.sh
@@ -4,28 +4,33 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 PWD=$(pwd)
 
-NVIM_DIR=$PWD/tmp/nvim
+NVIM_DIR=$HOME/tmp/nvim
 
 mkdir -p $NVIM_DIR
 
 if command -v apt &> /dev/null
 then
-  # Install our neovim reqs
-  if ! command -v tmux &> /dev/null
+  # If this is the first run, install requirements
+  if ! command -v $NVIM_DIR/nvim-linux64/bin/nvim &> /dev/null
   then
-    sudo apt install -y tmux
-  fi
-  # Needed for FZF building
-  if ! command -v cmake &> /dev/null
-  then
-    sudo apt install -y cmake
-  fi
-  if ! command -v rg &> /dev/null
-  then
-    RIPGREP_VERSION=$(curl -s "https://api.github.com/repos/BurntSushi/ripgrep/releases/latest" | grep -Po '"tag_name": "\K[0-9.]+')
-    curl -Lo ripgrep.deb "https://github.com/BurntSushi/ripgrep/releases/download/${RIPGREP_VERSION}/ripgrep_${RIPGREP_VERSION}-1_amd64.deb"
-    sudo apt install -y ./ripgrep.deb
-    rm ripgrep.deb
+	  sudo apt update
+	  # Install our neovim reqs
+	  if ! command -v tmux &> /dev/null
+	  then
+	    sudo apt install -y tmux
+	  fi
+	  # Needed for FZF building
+	  if ! command -v cmake &> /dev/null
+	  then
+	    sudo apt install -y cmake
+	  fi
+	  if ! command -v rg &> /dev/null
+	  then
+	    RIPGREP_VERSION=$(curl -s "https://api.github.com/repos/BurntSushi/ripgrep/releases/latest" | grep -Po '"tag_name": "\K[0-9.]+')
+	    curl -Lo ripgrep.deb "https://github.com/BurntSushi/ripgrep/releases/download/${RIPGREP_VERSION}/ripgrep_${RIPGREP_VERSION}-1_amd64.deb"
+	    sudo apt install -y ./ripgrep.deb
+	    rm ripgrep.deb
+	  fi
   fi
 fi
 
